0163a79c410507d039fad0b09ce0cec783663f23
João Pedro <jpedro1580@gmail.com>
2022-12-22 11:40:53 -0300

    Fim do desafio portfólio

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,42 +1,42 @@
 "use strict";
 
-const fetchUserData = async (user) => {
+async function fetchUserData(user) {
   const response = await fetch(`https://api.github.com/users/${user}`);
   return response.json();
-};
+}
 
-const fetchUserRepoData = async (user) => {
+async function fetchUserRepoData(user) {
   const response = await fetch(`https://api.github.com/users/${user}/repos`);
   return response.json();
-};
+}
 
-const hidrateImg = (userData, imageNode) => {
+function hidrateImg(userData, imageNode) {
   imageNode.src = userData.avatar_url;
-};
+}
 
-const hidrateName = (userData, nameNode) => {
+function hidrateName(userData, nameNode) {
   nameNode.innerText = userData.name;
-};
+}
 
-const toMap = (acc, item) => {
+function toMap(acc, item) {
   if (acc.has(item)) {
     acc.set(item, acc.get(item) + 1);
   } else {
     acc.set(item, 1);
   }
   return acc;
-};
+}
 
-const getTechnologies = async () => {
+async function getTechnologies() {
   const repoData = await fetchUserRepoData("JP-Go");
   const languages = repoData
     .map(({ language }) => language?.toUpperCase())
     .filter((item) => item)
     .reduce(toMap, new Map());
   return languages;
-};
+}
 
-const orderTechnologies = (techData) => {
+function orderTechnologies(techData) {
   const data = [];
   techData.forEach((_, tech) => {
     if (data[0] === undefined || techData.get(data[0]) > techData.get(tech)) {
@@ -46,9 +46,9 @@ const orderTechnologies = (techData) => {
     }
   });
   return data;
-};
+}
 
-const hidrateTechnologies = (techArray, domNode) => {
+function hidrateTechnologies(techArray, domNode) {
   const elements = techArray.map((tech) => {
     const span = document.createElement("span");
     span.innerText = tech;
@@ -58,7 +58,8 @@ const hidrateTechnologies = (techArray, domNode) => {
   elements.forEach((elem) => {
     domNode.appendChild(elem);
   });
-};
+}
+
 const hidrate = async () => {
   const userData = await fetchUserData("JP-Go");
   const techData = orderTechnologies(await getTechnologies());
